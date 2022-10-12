2430be1962eb37ec14fa3c8b7bbbe1831577b1ce
Niclas Trometer <Niclas.Trometer@gmx.de>
2022-10-12 18:15:02 +0200

    added index structure

---
+++ mywebsite/src/components/App.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Navbar from './Navbar.js'
+import Footer from './Footer.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mywebsite/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => {
+    return (
+        
+        <div className="navbar">
+            <div className="links">
+                <ul>
+                    <h1>Niclas Trometer</h1>
+                    <li><a href="#">About me</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </div>
+        </div>
+        
+    )
+}
