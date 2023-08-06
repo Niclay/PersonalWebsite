cdfc78cc066e9f1afe8ad3f4f8741ee3824a2ded
Niclas Trometer <Niclas.Trometer@gmx.de>
2023-08-06 23:47:27 +0200

    added Navbar & PersonalInfo

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <div className="nav__items">
+        <div className="nav__item">About</div>
+        <div className="nav__item">Skills</div>
+        <div className="nav__item">Projects</div>
+        <div className="nav__item">Qualifications</div>
+        <div className="nav__item">Contact</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/PersonalInfo.jsx
@@ -0,0 +1,15 @@
+export default function PersonalInfo() {
+  return (
+    <div className="personalInfoBody">
+      <div className="personalInfoCard">
+        <div className="personalInfoCard__text">
+          <div className="personalInfoCard__text__name">Niclas Trometer</div>
+          <div className="personalInfoCard__text__description">
+            Frontend-Developer
+          </div>
+        </div>
+        <div className="personalInfoCard__image"></div>
+      </div>
+    </div>
+  );
+}
